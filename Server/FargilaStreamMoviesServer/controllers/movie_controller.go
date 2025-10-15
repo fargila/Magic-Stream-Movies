03f8f87ec45fe8a45d1c80fbd633113203d6e56f
Atila <atilahunesco@gmail.com>
2025-10-15 13:31:34 -0300

    feat: finishing the get one singular movie logic and adding the addMovie function.

--- Server/FargilaStreamMoviesServer/controllers/movie_controller.go
+++ Server/FargilaStreamMoviesServer/controllers/movie_controller.go
@@ -9,11 +9,14 @@ import (
 	models "github.com/fargila/Magic-Stream-Movies/Server/FargilaStreamMoviesServer/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -58,5 +61,31 @@ func GetMovie() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input."})
+			return
+		}
+
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed.", "details": err.Error()})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add movie."})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
 	}
 }
