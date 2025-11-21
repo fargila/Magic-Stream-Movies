c953885e58d8ddf95dc4a1927fde463d6071a69c
Atila <atilahunesco@gmail.com>
2025-11-21 18:36:51 -0300

    feat: adding axios and Home page base to project

---
+++ Client/frontend/app/components/home/Home.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react"
+import axiosClient from '../../api/axiosConfig'
+import Movies from '../movies/Movies'
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
